01368201a552d8bb48f6c4633e266d289ab562cb
abusaleem <isahlabbo22@gmail.com>
2020-04-16 21:24:36 +0100

    modify the student dashboard

--- Modules/Student/Resources/views/index.blade.php
+++ Modules/Student/Resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <div class="col-md-12">
     <br>
     <div class="card shadow">
-        <div class="card-header shadow">
+        <div class="card-header shadow bt-color-1">
             <b class="text text-danger" >General Notification !!!</b> 
         </div>
         <div class="card-body">
@@ -13,8 +13,8 @@
     </div>
     <br>
     <div class="card shadow">
-        <div class="card-header shadow">
-            <b class="text text-danger" >Results Notification !!!</b> 
+        <div class="card-header shadow bt-color-1">
+            <b class="" >Results Notification !!!</b> 
         </div>
         <div class="card-body">
             
