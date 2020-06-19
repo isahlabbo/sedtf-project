40e6d51c85d1a2b72a104596b80f9615b1dc24ad
isahlabbo <isahlabbo22@gmail.com>
2020-06-19 18:18:20 +0100

    exam officer can check the individual student result

--- Modules/Coodinator/Resources/views/department/course/result/student/result.blade.php
+++ Modules/Coodinator/Resources/views/department/course/result/student/result.blade.php
@@ -1,29 +1,31 @@
 @extends('layouts.result')
 
 @section('page-content')
+
 	<div class="table-responsive">
-		@if($registration != null)
+		@if($registration->batch == null)
              <div class="alert alert-danger">There is no course registration foun for this student</div>
         @else
-	    <div class="col-md-12 text-center"><br><br>
+	    <div class="col-md-12"><br>
 	    	<table>
 	    		<tr>
 	    			<td>NAME</td>
 	    			<td>
-	    				{{strtoupper($registration->sessionRegistration->student->first_name)}} {{strtoupper($registration->sessionRegistration->student->middle_name)}}
-    		            {{strtoupper($registration->sessionRegistration->student->last_name)}}
+	    				{{strtoupper($registration->student->first_name)}} {{strtoupper($registration->student->middle_name)}}
+    		            {{strtoupper($registration->student->last_name)}}
     	            </td>
 	    		</tr>
 	    		<tr>
 	    			<td>PROGRAMME</td>
-	    			<td>{{strtoupper($registration->sessionRegistration->student->admission->programme->name)}}</td>
+	    			<td>{{strtoupper($registration->student->admission->programme->name)}}</td>
 	    		</tr>
 	    		<tr>
-	    			<td>ADMISSION NO</td>
-	    			<td>{{strtoupper($registration->sessionRegistration->student->admission->admission_no)}}</td>
+	    			<td width="200">ADMISSION NO</td>
+	    			<td>{{strtoupper($registration->student->admission->admission_no)}}</td>
 	    		</tr>
 	    	</table>
-	    </div>
+	    </div><br>
+
 		<table class="table table-bordered table-striped" >
             @include('coodinator::department.course.result.student.table.header')
             @include('coodinator::department.course.result.student.table.body')
